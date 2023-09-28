bb6dbb64973dc5d39651aecac6d1f923fb4cbde9
Favourdev <favourapps17@gmail.com>
2023-09-28 09:56:41 +0100

    updated templtate  component

--- src/Home/HomePage.vue
+++ src/Home/HomePage.vue
@@ -135,7 +135,7 @@
       </div>
 
       <div class="z-10 absolute w-full py-10">
-        <div class="h-full w-full md:w-11/12 lg:w-3/4 mx-auto">
+        <div class="h-full w-full md:w-11/12 xl:w-4/5 mx-auto">
           <div>
             <h5 class="text-blue-600 font-bold">Pricing</h5>
             <p class="text-5xl font-bold my-2">Get just one or get ’em all.</p>
@@ -145,150 +145,13 @@
               data with every click.
             </p>
           </div>
-<!-- payment card starts here -->
-          <div class="flex items-center mx-auto">
-            <div class="w-full"></div>
-            <div
-              class="relative z-10 -mx-4 shadow-lg ring-1 ring-slate-900/10 sm:mx-0 sm:rounded-3xl lg:w-1/2 lg:flex-none"
-            >
-              <div class="relative bg-white px-4 py-10 sm:rounded-3xl sm:px-10">
-                <div class="flex items-center justify-between">
-                  <p class="text-sky-700 font-bold text-medium">FREE</p>
+          <!-- payment card starts here -->
+          <div class="flex items-center mx-auto py-10">
+            <!-- <div class="w-full "></div> -->
 
-                  <button
-                    class="inline-flex justify-center rounded-lg text-sm font-medium py-2 px-3 bg-slate-900 text-white hover:bg-slate-700"
-                  >
-                    Get Started
-                  </button>
-                </div>
-
-                <div class="mt-3 flex items-center">
-                  <p class="text-[2.5rem] leading-none text-slate-900">
-                    $<span class="font-bold">299</span>
-                  </p>
-                  <p class="ml-3 text-sm">
-                    <span class="font-semibold text-slate-900"
-                      >one-time payment</span
-                    ><br /><span class="text-slate-500">plus local taxes</span>
-                  </p>
-                </div>
-
-                <p class="mt-6 text-sm leading-6 text-slate-600">
-                  Get lifetime access to every one of our website templates as
-                  well as all of our component examples for a single one-time
-                  purchase.
-                </p>
-
-                <ul
-                  class="mt-10 space-y-8 border-t border-slate-900/10 pt-10 text-sm leading-6 text-slate-700"
-                >
-                  <li class="flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 32 32"
-                      class="h-8 w-8 flex-none"
-                    >
-                      <path fill="#fff" d="M0 0h32v32H0z"></path>
-                      <rect
-                        width="23"
-                        height="22"
-                        x="3"
-                        y="5"
-                        stroke="#0EA5E9"
-                        stroke-linejoin="round"
-                        stroke-width="1.5"
-                        rx="2"
-                      ></rect>
-                      <rect
-                        width="10"
-                        height="18"
-                        x="19"
-                        y="9"
-                        fill="#E0F2FE"
-                        stroke="#0EA5E9"
-                        stroke-linejoin="round"
-                        stroke-width="1.5"
-                        rx="2"
-                      ></rect>
-                      <circle cx="6" cy="8" r="1" fill="#0EA5E9"></circle>
-                      <circle cx="9" cy="8" r="1" fill="#0EA5E9"></circle>
-                      <path
-                        stroke="#0EA5E9"
-                        stroke-width="1.5"
-                        d="M3 11h16"
-                      ></path>
-                    </svg>
-                    <p class="ml-5">
-                      <strong class="font-semibold text-slate-900"
-                        >Every site template</strong
-                      >
-                      — beautifully designed, expertly crafted website templates
-                      built with modern technologies like React and Next.js.
-                    </p>
-                  </li>
-                  <li class="flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 32 32"
-                      class="h-8 w-8 flex-none"
-                    >
-                      <path fill="#fff" d="M0 0h32v32H0z"></path>
-                      <path
-                        fill="#E0F2FE"
-                        d="M23 22l7-4v7l-7 4v-7zM9 22l7-4v7l-7 4v-7zM16 11l7-4v7l-7 4v-7zM2 18l7 4v7l-7-4v-7zM9 7l7 4v7l-7-4V7zM16 18l7 4v7l-7-4v-7z"
-                      ></path>
-                      <path
-                        fill="#0EA5E9"
-                        d="M16 3l.372-.651a.75.75 0 00-.744 0L16 3zm7 4h.75a.75.75 0 00-.378-.651L23 7zM9 7l-.372-.651A.75.75 0 008.25 7H9zM2 18l-.372-.651A.75.75 0 001.25 18H2zm28 0h.75a.75.75 0 00-.378-.651L30 18zm0 7l.372.651A.75.75 0 0030.75 25H30zm-7 4l-.372.651a.75.75 0 00.744 0L23 29zM9 29l-.372.651a.75.75 0 00.744 0L9 29zm-7-4h-.75c0 .27.144.518.378.651L2 25zM15.628 3.651l7 4 .744-1.302-7-4-.744 1.302zm7 2.698l-7 4 .744 1.302 7-4-.744-1.302zm-6.256 4l-7-4-.744 1.302 7 4 .744-1.302zm-7-2.698l7-4-.744-1.302-7 4 .744 1.302zm-.744 7l7 4 .744-1.302-7-4-.744 1.302zm7 2.698l-7 4 .744 1.302 7-4-.744-1.302zm-6.256 4l-7-4-.744 1.302 7 4 .744-1.302zm-7-2.698l7-4-.744-1.302-7 4 .744 1.302zm20.256-4l7 4 .744-1.302-7-4-.744 1.302zm7 2.698l-7 4 .744 1.302 7-4-.744-1.302zm-6.256 4l-7-4-.744 1.302 7 4 .744-1.302zm-7-2.698l7-4-.744-1.302-7 4 .744 1.302zm13.256 5.698l-7 4 .744 1.302 7-4-.744-1.302zm-6.256 4l-7-4-.744 1.302 7 4 .744-1.302zM30.75 25v-7h-1.5v7h1.5zm-15.122-.651l-7 4 .744 1.302 7-4-.744-1.302zm-6.256 4l-7-4-.744 1.302 7 4 .744-1.302zM2.75 25v-7h-1.5v7h1.5zm14 0v-7h-1.5v7h1.5zM8.25 7v7h1.5V7h-1.5zm14 0v7h1.5V7h-1.5zm-7 4v7h1.5v-7h-1.5zm-7 11v7h1.5v-7h-1.5zm14 0v7h1.5v-7h-1.5z"
-                      ></path>
-                    </svg>
-                    <p class="ml-5">
-                      <strong class="font-semibold text-slate-900"
-                        >Over 500+ components</strong
-                      >
-                      — everything you need to build beautiful application UIs,
-                      marketing sites, ecommerce stores, and more.
-                    </p>
-                  </li>
-                  <li class="flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 32 32"
-                      class="h-8 w-8 flex-none"
-                    >
-                      <path fill="#fff" d="M0 0h32v32H0z"></path>
-                      <path
-                        fill="#E0F2FE"
-                        d="M13.168 18.828a4 4 0 110-5.656L15.997 16l1.5-1.5 1.328-1.328a4 4 0 110 5.656L15.996 16l-1.499 1.5-1.329 1.328z"
-                      ></path>
-                      <path
-                        stroke="#0EA5E9"
-                        stroke-linecap="round"
-                        stroke-width="1.5"
-                        d="M14.497 17.5l-1.329 1.328a4 4 0 110-5.656l5.657 5.656a4 4 0 100-5.656L17.496 14.5"
-                      ></path>
-                      <circle
-                        cx="16"
-                        cy="16"
-                        r="14"
-                        stroke="#0EA5E9"
-                        stroke-width="1.5"
-                      ></circle>
-                    </svg>
-                    <p class="ml-5">
-                      <strong class="font-semibold text-slate-900"
-                        >Lifetime access</strong
-                      >
-                      — get instant access to everything we have today, plus any
-                      new components and templates we add in the future.
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <PaymentContainer></PaymentContainer>
+            <PaymentContainer></PaymentContainer>
+            <PaymentContainer></PaymentContainer>
           </div>
         </div>
       </div>
@@ -297,7 +160,11 @@
 </template>
 
 <script>
+import PaymentContainer from ".././components/Payment/PaymentComponent.vue";
 export default {
+  components: {
+    PaymentContainer,
+  },
   data() {
     return {
       isFocused: false,
