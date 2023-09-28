d2dccf462cb30be704d721c5c1f47bacea666cc8
Favourdev <favourapps17@gmail.com>
2023-09-28 08:45:18 +0100

    added new section

--- src/Home/HomePage.vue
+++ src/Home/HomePage.vue
@@ -79,10 +79,58 @@
       </div>
 
       <div>
-        <img
-          :src="require('@/assets/images/linkIllustation.svg')"
-          alt=""
-        />
+        <img :src="require('@/assets/images/linkIllustation.svg')" alt="" />
+      </div>
+    </div>
+    <div class="mt-10 py-20 text-start">
+      <div class="w-1/2">
+        <div
+          class="w-16 h-16 p-1 my-7 rounded-full ring-1 ring-slate-900/10 shadow overflow-hidden"
+        >
+          <img
+            :src="require('@/assets/images/modern-features.0c791977.png')"
+            class="bg-contain w-18 rounded-full"
+            alt=""
+          />
+        </div>
+
+        <h5 class="text-blue-600 font-bold">Analytics & Marketing Reporting</h5>
+        <p class="text-3xl font-bold my-2">
+          Understand what’s resonating with your audience
+        </p>
+        <p class="text-slate-600 text-lg lg:pr">
+          Whether you’re sharing one link or millions, UrlShort lets you
+          personalize, share, and track your content links, while capturing data
+          with every click.
+        </p>
+
+        <div
+          class="rounded-full bg-blue-50 hover:bg-blue-100 hover:text-blue-700 text-blue-600 w-max px-6 my-3 text-sm font-bold py-3 flex items-center cursor-pointer"
+        >
+          <p>Get Started</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div>
+        <img :src="require('@/assets/images/urlshort.png')" alt="" />
+      </div>
+    </div>
+
+    <div class="relative mt-10 py-20">
+      <div class="absolute w-full h-full">
+        <img :src="require('@/assets/images/beams.png')" alt="" />
       </div>
     </div>
   </div>
